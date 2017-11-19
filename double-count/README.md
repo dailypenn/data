615d6ad56f38ecabbd30a1ec752b749cc7d5f3e2
Andrew Fischer <afischer15@mac.com>
2017-11-19 18:52:18 -0500

    Add doublecount data

---
+++ double-count/README.md
@@ -0,0 +1,25 @@
+# Double Counting Courses
+
+The data in this folder contains information about courses that double count
+across the sectors and foundations. Data prior to 2017s was created using a
+crazy Excel macros, but the python file in this repo should be used for all future
+data collection.
+
+## TODO:
+- [ ] Merge course-codes for duplicate courses in code
+- [ ] Use PennLabs PennCourseReview API to get average score, difficulties
+
+## Inspiration
+This project was inspired by the now-defunct
+[Double Count at UPenn](http://doublecountatupenn.weebly.com), which stopped
+posting in Spring '15 when creator Sara Allan graduated.
+
+## Relevant Links
+#### Articles
+- [Fall 2017](http://www.thedp.com/article/2017/03/double-count-classes-fall-2017)
+- [Spring 2017](http://www.thedp.com/article/2016/10/double-count-classes-spring-2017)
+
+#### Links
+- [Policies Governing Double Counting Courses](https://www.college.upenn.edu/double-counting-courses)
+- [http://pennlabs.org/console/#depts](PCR API Explorer)
+  - [PCR API Docs](./PCR_API_DOCS.md)
---
+++ double-count/doubleCountScraper.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+
+BASE_URL = "http://fission.sas.upenn.edu/col/genreq/results.php?"
+
+SECTORS = {
+    "I: Society"                       : "req[]=S&cls=10",
+    "II: History and Tradition"        : "req[]=H&cls=10",
+    "III: Arts and Letters"            : "req[]=A&cls=10",
+    "IV: Humanities and Social Science": "req[]=O&req[]=B&cls=10",
+    "V: Living World"                  : "req[]=L&cls=10",
+    "VI: Physical World"               : "req[]=P&cls=10",
+    "VII: Natural Sciences and Math"   : "req[]=N&req[]=B&cls=10",
+    "Quantitative Data Analysis"       : "type=M&cls=10",
+    "Formal Reasoning & Analysis"      : "type=FR&cls=10",
+    "Cross Cultural Analysis"          : "type=C1&cls=10",
+    "Cultural Diversity in the U.S."   : "type=C2&cls=10",
+    "Language"                         : "req[]=M&cls=10"
+}
+
+FOUNDATIONS = {
+
+}
+
+course_names = {}
+course_map = {}
+course_data = {}
+double_counts = {}
+# ADD WRITING
+
+def getData(url, req_name):
+    r = requests.get(url)
+
+    data = r.text
+    soup = bs(data, "html.parser")
+    table = soup.find('table', class_="cell2010")
+
+    for row in table.findAll('tr')[1:]:  # strip header row
+        cols = row.findAll('td')[1:]     # strip spacer column
+        for col in cols:
+            name = cols[0].text
+            code = cols[1].text
+            # add course code to name mapping
+            course_names[code] = name
+            # if name in course_map.keys():
+            #     course_map[name].add(code)
+            # else:
+            #     course_map[name] = {code}
+            # add what course fufills
+            if code not in course_data.keys():
+                course_data[code] = {req_name}
+            else:
+                course_data[code].add(req_name)
+
+def printSet(set):
+    return ', '.join(set)
+
+for name, querystr in SECTORS.iteritems():
+    print "doing ", name
+    getData(BASE_URL + querystr, name)
+    # print course_data
+
+with open('double_count.csv', 'wb') as csvfile:
+    csv = csv.writer(csvfile)
+    # find where multiple reqs are satisfied, combine over name
+    for code, reqs in course_data.iteritems():
+        if len(course_data[code]) > 1:
+            csv.writerow([course_names[code], code, printSet(course_data[code])])
