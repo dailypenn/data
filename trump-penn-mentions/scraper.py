334bc11da3e1d2f5ac22674e8ae880db02266fcc
Alex Graves <graves@thedp.com>
2018-01-15 20:55:39 -0500

    clean ivy league, add good student

--- trump-penn-mentions/scraper.py
+++ trump-penn-mentions/scraper.py
@@ -7,17 +7,34 @@ query = sys.argv[1]
 url = 'https://factba.se/json/json-20170612.php'
 
 # get results from the date Trump announced his candidacy to today
+page = 1
 params = dict(
     q=query,
     startdate='2015-06-16',
     enddate=datetime.date.today(),
     l=200,
-    p=1
+    p=page
 )
 
 r = requests.get(url, params)
 results = json.loads(r.text)
 data = results['combo']['data']
+num = results['combo']['filtered']
+
+# go through all pages if there are more than 200 results
+while num > 200:
+    page += 1
+    params = dict(
+        q=query,
+        startdate='2015-06-16',
+        enddate=datetime.date.today(),
+        l=200,
+        p=page
+    )
+    r = requests.get(url, params)
+    results = json.loads(r.text)
+    data.extend(results['combo']['data'])
+    num -= 200
 
 with open(query.replace(' ', '_') + '_mentions.csv', 'wb') as output:
     csv = csv.writer(output)
@@ -26,12 +43,12 @@ with open(query.replace(' ', '_') + '_mentions.csv', 'wb') as output:
     'Disgust Score', 'Joy Score', 'Fear Score', 'Sadness Score'])
 
     for d in data:
-        has_emotion = len(d['emotion']) > 0
-        has_sentiment = len(d['sentiment']) > 0
+        has_emotion = 'emotion' in d and len(d['emotion']) > 0
+        has_sentiment = 'sentiment' in d and len(d['sentiment']) > 0
 
         csv.writerow([d['text'].encode('utf8'), d['date'], d['place'],
-        d['source'] if d['record_type'] != 'Twitter' else 'Tweet',
-        d['record_type'], d['url'],
+        d['source'] if 'source' in d else 'Tweet' if d['record_type'] != 'Twitter' else 'Unknown',
+        d['record_type'] if 'record_type' in d else d['type'], d['url'],
         d['sentiment'][0]['text'] if has_sentiment else '',
         d['sentiment'][0]['score'] if has_sentiment else '',
         d['emotion'][5]['text'] if has_emotion else '',
