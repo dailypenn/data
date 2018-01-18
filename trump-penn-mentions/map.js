b78bac64d4be60400f1ad71472c71139c074e714
Alex Graves <graves@thedp.com>
2018-01-17 20:51:33 -0500

    fix tooltip placement

--- trump-penn-mentions/map.js
+++ trump-penn-mentions/map.js
@@ -117,8 +117,8 @@ function ready(us) {
       sel.moveToFront();
       d3.select(this).transition().duration(300).style('opacity', 0.8);
       tooltip.transition().duration(300).style('opacity', 1)
-      tooltip.style('left', (d3.event.pageX) + 'px')
-        .style('top', (d3.event.pageY - 10) + 'px');
+      tooltip.style('left', (d3.mouse(this)[0]) + 'px')
+        .style('top', (d3.mouse(this)[1] - 10) + 'px');
       tooltip.select('p')
         .text(stateByID[d.id] + ': '+ valuesByID[d.id] + (valuesByID[d.id] != 1 ? ' mentions' : ' mention'));
     })
