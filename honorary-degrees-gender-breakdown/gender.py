2335962ea570d56af5acbbf00f46f32ef9c108c9
Alex Graves <graves@thedp.com>
2018-02-12 21:07:18 -0500

    add another gender library for better guessing

--- honorary-degrees-gender-breakdown/gender.py
+++ honorary-degrees-gender-breakdown/gender.py
@@ -1,13 +1,17 @@
 import csv
 import gender_guesser.detector as gender
 d = gender.Detector()
+from genderizer.genderizer import Genderizer as g
 
 with open('honorees_clean.csv', 'rb') as honorees, open('genders.csv', 'wb') as output:
     r = csv.reader(honorees)
     w = csv.writer(output)
 
+    w.writerow(['Name', 'Gender Guesser', 'Genderizer'])
+
     for row in r:
         name = row[0]
         names = name.split(' ')
         gender = d.get_gender(names[0])
-        w.writerow([name, gender])
+        gender2 = g.detect(firstName=names[0])
+        w.writerow([name, gender, 'unknown' if gender2 is None else gender2])
