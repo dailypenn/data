c09fc8ea151b98b2b76aa13f08f324d4b96a2c17
Daniel Salib <dsalib@sas.upenn.edu>
2018-02-13 20:29:11 -0500

    ivy-league-breaks added

---
+++ ivy-league-breaks/counter.py
@@ -0,0 +1,16 @@
+
+import json, csv
+from datetime import timedelta, date
+
+with open("columbia-breaks-2018.json") as json_data, open("out.csv", "w") as output:
+    writer = csv.writer(output)
+    data = json.load(json_data)
+    for p in data:
+        year_start = int(p['start'][0:3])
+        year_end = int(p['end'][0:3])
+        month_start = int(p['start'][4:6])
+        month_end = int(p['end'][4:6])
+        day_start = int(p['start'][6:8])
+        day_end = int(p['end'][6:8])
+        #print day_end
+        writer.writerow([p['summary'],(date(year_end, month_end, day_end) - date(year_start, month_start, day_start)).days])
