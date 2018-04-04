fb58cc4492bcb8750e8f6b5aa06b0727c43a4c21
Ishaan Rao <ishdr08@gmail.com>
2018-04-03 20:01:00 -0400

    Timeline update

--- ivy-league-breaks/charts/charts.js
+++ ivy-league-breaks/charts/charts.js
@@ -2,14 +2,14 @@ var winter = c3.generate({
   bindto: '#winter',
   data: {
       columns: [
-        ['Days Off', 0, 20, 23, 24, 26, 32, 40, 41]
+        ['Days Off', 20, 23, 24, 26, 32, 32, 40, 41]
       ],
       type: 'bar',
       colors: {
         'Days Off': '#000000'
       },
       color: function(c, data) {
-        var colors = ['#A51C30', '#01256E', '#EE7F2D', '#C4D8E2', '#00356B', '#4E3629', '#B31B1B', '#00693E'];
+        var colors = ['#01256E', '#EE7F2D', '#C4D8E2', '#00356B', '#A51C30', '#4E3629', '#B31B1B', '#00693E'];
         if (data.index !== undefined) return colors[data.index];
         return c;
       }
@@ -17,7 +17,7 @@ var winter = c3.generate({
   axis: {
     x: {
       type: 'category',
-      categories: ['Harvard', 'Penn', 'Princeton', 'Columbia',  'Yale', 'Brown', 'Cornell', 'Dartmouth']
+      categories: ['Penn', 'Princeton', 'Columbia',  'Yale', 'Harvard', 'Brown', 'Cornell', 'Dartmouth']
     },
     y: {
       label: {
@@ -38,14 +38,14 @@ var summer = c3.generate({
   bindto: '#summer',
   data: {
       columns: [
-        ['Days Off', 0, 93, 98, 109, 109, 111, 112, 116]
+        ['Days Off', 93, 98, 109, 109, 111, 112, 115, 116]
       ],
       type: 'bar',
       colors: {
         'Days Off': '#000000'
       },
       color: function(c, data) {
-        var colors = ['#A51C30', '#B31B1B', '#00693E', '#EE7F2D', '#4E3629', '#00356B', '#01256E', '#C4D8E2'];
+        var colors = ['#B31B1B', '#00693E', '#EE7F2D', '#4E3629', '#00356B', '#01256E', '#A51C30', '#C4D8E2'];
         if (data.index !== undefined) return colors[data.index];
         return c;
       }
@@ -53,7 +53,7 @@ var summer = c3.generate({
   axis: {
     x: {
       type: 'category',
-      categories: ['Harvard', 'Cornell', 'Dartmouth', 'Princeton', 'Brown', 'Yale', 'Penn', 'Columbia']
+      categories: ['Cornell', 'Dartmouth', 'Princeton', 'Brown', 'Yale', 'Penn', 'Harvard', 'Columbia']
     },
     y: {
       label: {
